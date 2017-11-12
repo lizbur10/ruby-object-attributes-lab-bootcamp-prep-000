d563770b8768543f9c0f2e043091c1bbdc37955d
Liz Burton <liz.burton147@gmail.com>
2017-11-12 16:38:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
-  def name=(dog_name)
-    @name = dog_name
+  def name=(dogs_name)
+    @name = dogs_name
   end
 
   def name
     @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
   end
 end
